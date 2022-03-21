a3c43d745be8e2f88248660fcf4fdc3ed2c05926
Josh Bedwell <rcxwhiz@gmail.com>
2022-03-21 17:51:49 -0600

    Added fish tank and fish species models

--- websiteApp/admin.py
+++ websiteApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Project, PreviousJob, BlogPost, CourseTaken
+from .models import Project, PreviousJob, BlogPost, CourseTaken, FishSpecies, FishTank
 
 admin.site.register(Project)
 admin.site.register(PreviousJob)
 admin.site.register(BlogPost)
 admin.site.register(CourseTaken)
+admin.site.register(FishSpecies)
+admin.site.register(FishTank)
